ae73c718146d07e1d933faaa99322ee4c35989a6
maxial <alievmaxx@gmail.com>
2023-01-22 20:18:41 +0300

    add swiftlint package dependency

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "NetHub",
     platforms: [
-        .macOS(.v10_15), .iOS(.v15)
+        .macOS(.v12), .iOS(.v15)
     ],
     products: [
         .library(name: "NetHub", targets: ["NetHub"]),
